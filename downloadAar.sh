899b55b1167b6922c2851b58710e54dbfbf54fd3
Nigel Birkenshaw <nbirkenshaw@mux.com>
2023-04-06 15:05:49 -0400

    Changes to use artifactory

--- downloadAar.sh
+++ downloadAar.sh
@@ -5,7 +5,10 @@ set -e
 # Get VERSION_NAME from gradle.properties
 VERSION=`grep -o 'VERSION_NAME=.*' gradle.properties | cut -f2- -d=`
 
-SDK_BIN_URL=https://github.com/webrtc-sdk/android/releases/download/${VERSION}/libwebrtc.aar
+SDK_BIN_URL=https://github.com/atomirex/webrtc-build/releases/download/m${VERSION}/webrtc.android.tar.gz
 
 echo "Downloading webrtc-sdk ${VERSION} binary for android."
 curl -L -O ${SDK_BIN_URL}
+
+tar -xzvf webrtc.android.tar.gz webrtc/aar/libwebrtc.aar
+mv webrtc/aar/libwebrtc.aar libwebrtc.aar
