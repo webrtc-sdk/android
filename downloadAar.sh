24bcaa32d68e0d0755e4e7e5d3679f2222584d63
davidliu <davidliu@deviange.net>
2023-04-25 19:08:59 +0900

    Conform to new tagging format
    Also fail if error status code

--- downloadAar.sh
+++ downloadAar.sh
@@ -5,7 +5,7 @@ set -e
 # Get VERSION_NAME from gradle.properties
 VERSION=`grep -o 'VERSION_NAME=.*' gradle.properties | cut -f2- -d=`
 
-SDK_BIN_URL=https://github.com/webrtc-sdk/android/releases/download/${VERSION}/libwebrtc.aar
+SDK_BIN_URL=https://github.com/webrtc-sdk/android/releases/download/v${VERSION}/libwebrtc.aar
 
 echo "Downloading webrtc-sdk ${VERSION} binary for android."
-curl -L -O ${SDK_BIN_URL}
+curl -f -L -O ${SDK_BIN_URL}
