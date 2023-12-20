4def5b35d06482ae880659183bf7de911a77344d
davidliu <davidliu@deviange.net>
2023-12-19 16:19:23 -0800

    Prefix so files to avoid collison

--- downloadAar_prefixed.sh
+++ downloadAar_prefixed.sh
@@ -12,15 +12,21 @@ echo "Downloading webrtc-sdk ${VERSION} prefixed binary for android."
 curl -f -L -O ${SDK_BIN_URL}
 
 # The prefixed aar can't be used as is.
-# The .so libs are already prefixed, but the jar itself
-# needs to be shadowed to properly add on the prefix.
+# The jar itself needs to be shadowed to properly add on the prefix.
 unzip -o libwebrtc_prefixed.aar -d prefix/
 rm -rf android-prefixed/shadow/libs/*
 mkdir -p android-prefixed/shadow/libs
 mv prefix/classes.jar android-prefixed/shadow/libs/
+
+# Copy the .so files to the main project for inclusion.
 rm -rf android-prefixed/src/main/jniLibs/*
 mkdir -p android-prefixed/src/main/jniLibs
 mv prefix/jni/* android-prefixed/src/main/jniLibs
+
+# Rename the so files to liblkjingle_peerconnection_so.so
+find android-prefixed/src/main/jniLibs/ -type f \
+  -name "libjingle_peerconnection_so.so" \
+  -exec sh -c 'f="{}"; mv -- "$f" "${f%libjingle_peerconnection_so.so}liblkjingle_peerconnection_so.so"' \;
 
 # clean up unzipped prefix files
 rm -rf prefix/
--- prefixmove.sh
+++ prefixmove.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+# Similar script to downloadAar_prefixed.sh, without the download part.
+# For use with local testing aars.
 
 set -e
 set -x
 
+# The prefixed aar can't be used as is.
+# The jar itself needs to be shadowed to properly add on the prefix.
 unzip -o libwebrtc_prefixed.aar -d prefix/
 rm -rf android-prefixed/shadow/libs/*
 mkdir -p android-prefixed/shadow/libs
 mv prefix/classes.jar android-prefixed/shadow/libs/
+
+# Copy the .so files to the main project for inclusion.
 rm -rf android-prefixed/src/main/jniLibs/*
 mkdir -p android-prefixed/src/main/jniLibs
 mv prefix/jni/* android-prefixed/src/main/jniLibs
+
+# Rename the so files to liblkjingle_peerconnection_so.so
+find android-prefixed/src/main/jniLibs/ -type f \
+  -name "libjingle_peerconnection_so.so" \
+  -exec sh -c 'f="{}"; mv -- "$f" "${f%libjingle_peerconnection_so.so}liblkjingle_peerconnection_so.so"' \;
 
 # clean up unzipped prefix files
 rm -rf prefix/
